e0891d428018310fda1774c8da4a18b7f649d223
David Streid <david.streid.nyc@gmail.com>
2020-03-23 11:21:10 -0500

    removing book

--- project-tracker-backend/routes/api.js
+++ project-tracker-backend/routes/api.js
@@ -1,13 +1,11 @@
 var express = require("express");
 var authRouter = require("./auth");
-var bookRouter = require("./book");
 var limsRouter = require("./projects");
 
 var app = express();
 
 
 app.use("/auth/", authRouter);
-app.use("/book/", bookRouter);
 app.use("/projects/", limsRouter);
 
 module.exports = app;
