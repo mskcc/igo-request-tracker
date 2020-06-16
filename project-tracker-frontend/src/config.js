35335060c5a7722d91813a95c9aa2c47d6de28f4
David Streid <david.streid.nyc@gmail.com>
2020-06-16 10:11:05 -0400

    login_page url change

--- project-tracker-frontend/src/config.js
+++ project-tracker-frontend/src/config.js
@@ -3,4 +3,4 @@
 // export const LOGIN_PAGE_URL = "/login";
 export const HOME_PAGE_PATH = "";
 export const PROJECTS_ENDPOINT = "http://localhost:4000/api/projects";
-export const LOGIN_PAGE_URL = "http://localhost:3200/";
+export const LOGIN_PAGE_URL = "http://localhost:3000/";
