5d73cb8efd3401ba2b5aed1a17aefe994ccb0453
David Streid <david.streid.nyc@gmail.com>
2020-09-21 10:01:39 -0400

    test-fix

--- project-tracker-frontend/src/App.test.js
+++ project-tracker-frontend/src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
 import App from './App';
+import reducer from './redux/reducers/index'
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const { getByText } = render(<Provider store={createStore(reducer, {})}><App /></Provider>);
+  const linkElement = getByText(/IGO Request Tracker/i);
   expect(linkElement).toBeInTheDocument();
 });
