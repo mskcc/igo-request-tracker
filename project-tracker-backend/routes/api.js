50de76df39cff699e3a9b95f124d710827cde9a0
David Streid <david.streid.nyc@gmail.com>
2020-09-21 08:47:07 -0400

    Removing feedback controller

--- project-tracker-backend/routes/api.js
+++ project-tracker-backend/routes/api.js
@@ -1,10 +1,8 @@
 var express = require("express");
 var limsRouter = require("./projects");
-var feedbackRouter = require("./feedback");
 
 var app = express();
 
 app.use("/projects/", limsRouter);
-app.use("/feedback/", feedbackRouter);
 
 module.exports = app;
--- project-tracker-backend/routes/feedback.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-const FeedbackController = require("../controllers/feedback-controller");
-var router = express.Router();
-
-router.post("/submit", FeedbackController.submitFeedback);
-router.get("/get", FeedbackController.getFeedback);
-
-module.exports = router;
