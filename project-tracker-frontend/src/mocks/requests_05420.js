2baf9e935714810ee221c2ba1e811f80a4737956
David Streid <david.streid.nyc@gmail.com>
2021-02-25 17:48:49 -0500

    Adding concentration, mass, and volume for DNA & library

---
+++ project-tracker-frontend/src/mocks/requests_05420.js
@@ -0,0 +1,323 @@
+const resp = {
+    "status": 1,
+    "message": "success",
+    "data": {
+        "summary": {
+            "isDelivered": true,
+            "total": 8,
+            "RecentDeliveryDate": 1572379864950,
+            "stagesComplete": true,
+            "pendingStage": "Completed",
+            "isIgoComplete": true,
+            "completed": 8,
+            "failed": 0,
+            "CompletedDate": null
+        },
+        "metaData": {
+            "childRequests": [],
+            "ProjectName": " Identifying Biomarkers for Colorectal Tumor Response to Chemoradiation",
+            "LaboratoryHead": "Julio Garcia Aguilar",
+            "RequestName": "WholeExome-KAPALib",
+            "TATFromInProcessing": "",
+            "Investigator": "Chin-Tung Chen",
+            "ProjectManager": "",
+            "ReceivedDate": 1567626453496,
+            "TATFromReceiving": "",
+            "GroupLeader": "",
+            "requestId": "05240_W",
+            "sourceRequests": [],
+            "LabHeadEmail": "garciaaj@mskcc.org",
+            "DueDate": 1569988800000,
+            "serviceId": "IGO-015122"
+        },
+        "requestId": "05240_W",
+        "stages": [
+            {
+                "stage": "Submitted",
+                "completedSamples": 8,
+                "totalSamples": 8,
+                "startTime": null,
+                "updateTime": null,
+                "complete": true,
+                "failedSamples": 0
+            },
+            {
+                "stage": "Library Capture",
+                "completedSamples": 8,
+                "totalSamples": 8,
+                "startTime": 1567626102863,
+                "updateTime": 1571416762742,
+                "complete": true,
+                "failedSamples": 0
+            },
+            {
+                "stage": "Sequencing",
+                "completedSamples": 8,
+                "totalSamples": 8,
+                "startTime": 1570199181890,
+                "updateTime": 1572460988109,
+                "complete": true,
+                "failedSamples": 0
+            }
+        ],
+        "samples": [
+            {
+                "sampleId": 5822187,
+                "stages": [
+                    {
+                        "stage": "Library Capture",
+                        "startTime": 1567626102863,
+                        "updateTime": 1571156058149,
+                        "complete": true
+                    },
+                    {
+                        "stage": "Sequencing",
+                        "startTime": 1570199181890,
+                        "updateTime": 1571926460130,
+                        "complete": true
+                    }
+                ],
+                "sampleInfo": {
+                    "library_material": {
+                        "volume": 8,
+                        "mass": 400,
+                        "concentration": 50,
+                        "concentrationUnits": "ng/uL"
+                    },
+                    "dna_material": {
+                        "volume": 0,
+                        "mass": 0,
+                        "concentration": 0,
+                        "concentrationUnits": ""
+                    }
+                },
+                "status": "Complete"
+            },
+            {
+                "sampleId": 5822192,
+                "stages": [
+                    {
+                        "stage": "Library Capture",
+                        "startTime": 1567626103351,
+                        "updateTime": 1571156058149,
+                        "complete": true
+                    },
+                    {
+                        "stage": "Sequencing",
+                        "startTime": 1570199181917,
+                        "updateTime": 1571926460130,
+                        "complete": true
+                    }
+                ],
+                "sampleInfo": {
+                    "library_material": {
+                        "volume": 5.882352941176471,
+                        "mass": 250.00000000000003,
+                        "concentration": 42.5,
+                        "concentrationUnits": "ng/uL"
+                    },
+                    "dna_material": {
+                        "volume": 0,
+                        "mass": 0,
+                        "concentration": 0,
+                        "concentrationUnits": ""
+                    }
+                },
+                "status": "Complete"
+            },
+            {
+                "sampleId": 5822197,
+                "stages": [
+                    {
+                        "stage": "Library Capture",
+                        "startTime": 1567626103762,
+                        "updateTime": 1571416762742,
+                        "complete": true
+                    },
+                    {
+                        "stage": "Sequencing",
+                        "startTime": 1570199181944,
+                        "updateTime": 1572460988109,
+                        "complete": true
+                    }
+                ],
+                "sampleInfo": {
+                    "library_material": {
+                        "volume": 11.228070175438596,
+                        "mass": 256,
+                        "concentration": 22.8,
+                        "concentrationUnits": "ng/uL"
+                    },
+                    "dna_material": {
+                        "volume": 0,
+                        "mass": 0,
+                        "concentration": 0,
+                        "concentrationUnits": ""
+                    }
+                },
+                "status": "Complete"
+            },
+            {
+                "sampleId": 5822202,
+                "stages": [
+                    {
+                        "stage": "Library Capture",
+                        "startTime": 1567626104172,
+                        "updateTime": 1571156058149,
+                        "complete": true
+                    },
+                    {
+                        "stage": "Sequencing",
+                        "startTime": 1570199181971,
+                        "updateTime": 1571926460130,
+                        "complete": true
+                    }
+                ],
+                "sampleInfo": {
+                    "library_material": {
+                        "volume": 5.33420365535248,
+                        "mass": 204.29999999999998,
+                        "concentration": 38.3,
+                        "concentrationUnits": "ng/uL"
+                    },
+                    "dna_material": {
+                        "volume": 0,
+                        "mass": 0,
+                        "concentration": 0,
+                        "concentrationUnits": ""
+                    }
+                },
+                "status": "Complete"
+            },
+            {
+                "sampleId": 5822206,
+                "stages": [
+                    {
+                        "stage": "Library Capture",
+                        "startTime": 1567626104532,
+                        "updateTime": 1571416762742,
+                        "complete": true
+                    },
+                    {
+                        "stage": "Sequencing",
+                        "startTime": 1570199181998,
+                        "updateTime": 1572460988109,
+                        "complete": true
+                    }
+                ],
+                "sampleInfo": {
+                    "library_material": {
+                        "volume": 9.9281045751634,
+                        "mass": 151.90000000000003,
+                        "concentration": 15.3,
+                        "concentrationUnits": "ng/uL"
+                    },
+                    "dna_material": {
+                        "volume": 0,
+                        "mass": 0,
+                        "concentration": 0,
+                        "concentrationUnits": ""
+                    }
+                },
+                "status": "Complete"
+            },
+            {
+                "sampleId": 5822211,
+                "stages": [
+                    {
+                        "stage": "Library Capture",
+                        "startTime": 1567626104916,
+                        "updateTime": 1571416762742,
+                        "complete": true
+                    },
+                    {
+                        "stage": "Sequencing",
+                        "startTime": 1570199182025,
+                        "updateTime": 1572460988109,
+                        "complete": true
+                    }
+                ],
+                "sampleInfo": {
+                    "library_material": {
+                        "volume": 10,
+                        "mass": 200,
+                        "concentration": 20,
+                        "concentrationUnits": "ng/uL"
+                    },
+                    "dna_material": {
+                        "volume": 0,
+                        "mass": 0,
+                        "concentration": 0,
+                        "concentrationUnits": ""
+                    }
+                },
+                "status": "Complete"
+            },
+            {
+                "sampleId": 5822215,
+                "stages": [
+                    {
+                        "stage": "Library Capture",
+                        "startTime": 1567626105352,
+                        "updateTime": 1571416762742,
+                        "complete": true
+                    },
+                    {
+                        "stage": "Sequencing",
+                        "startTime": 1570199182053,
+                        "updateTime": 1572460988109,
+                        "complete": true
+                    }
+                ],
+                "sampleInfo": {
+                    "library_material": {
+                        "volume": 12.452830188679243,
+                        "mass": 329.99999999999994,
+                        "concentration": 26.5,
+                        "concentrationUnits": "ng/uL"
+                    },
+                    "dna_material": {
+                        "volume": 0,
+                        "mass": 0,
+                        "concentration": 0,
+                        "concentrationUnits": ""
+                    }
+                },
+                "status": "Complete"
+            },
+            {
+                "sampleId": 5822220,
+                "stages": [
+                    {
+                        "stage": "Library Capture",
+                        "startTime": 1567626105780,
+                        "updateTime": 1571416762742,
+                        "complete": true
+                    },
+                    {
+                        "stage": "Sequencing",
+                        "startTime": 1570199182080,
+                        "updateTime": 1572460988109,
+                        "complete": true
+                    }
+                ],
+                "sampleInfo": {
+                    "library_material": {
+                        "volume": 13.15068493150685,
+                        "mass": 384,
+                        "concentration": 29.2,
+                        "concentrationUnits": "ng/uL"
+                    },
+                    "dna_material": {
+                        "volume": 0,
+                        "mass": 0,
+                        "concentration": 0,
+                        "concentrationUnits": ""
+                    }
+                },
+                "status": "Complete"
+            }
+        ]
+    }
+};
+export default resp;
