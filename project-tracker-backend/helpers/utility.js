bf235a69256e66a2e9e4f6495873fef4e38fdbfb
Mirhaj <f.mirhaj@gmail.com>
2023-01-23 16:02:37 -0500

    Semantic error fixed.

--- project-tracker-backend/helpers/utility.js
+++ project-tracker-backend/helpers/utility.js
@@ -102,16 +102,18 @@ const getSavedUserGroups = async (username) => {
  */
 const getAccessGroups = (request) => {
 	const dataAccessEmailsValue = request["dataAccessEmails"] || "";
-	const qcAccessEmailsValue = request["qcAccessEmail"] || "";
-	const PIemail = request["labHeadEmail"] || "";
-	const InvestigatorEmail = request["investigatorEmail"] || "";
+        const qcAccessEmailsValue = request["qcAccessEmail"] || "";
+        const PIemail = request["labHeadEmail"] || "";
+        const InvestigatorEmailValue = request["investigatorEmail"] || "";
 
-	console.log("labHeadEmail: " + PIemail);
-	console.log("InvestigatorEmail: " + InvestigatorEmail);
-	const dataAccessEmails = dataAccessEmailsValue.split(",");
-	const qcAccessEmails = qcAccessEmailsValue.split(",");
+        console.log("labHeadEmail: " + PIemail);
+        console.log("InvestigatorEmail: " + InvestigatorEmailValue);
+        const dataAccessEmails = dataAccessEmailsValue.split(",");
+        const qcAccessEmails = qcAccessEmailsValue.split(",");
+        const labHeadEmail = PIemail.split(",");
+        const investEmail = InvestigatorEmailValue.split(",");
 
-	const accessEmails = PIemail.concat(InvestigatorEmail, dataAccessEmails, qcAccessEmails);
+        const accessEmails = labHeadEmail.concat(investEmail, dataAccessEmails, qcAccessEmails);
 	const accessGroups = [];
 	const unrecognizedEmails = new Set();
 	for(const emailValue of accessEmails){
